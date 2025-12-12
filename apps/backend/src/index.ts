e9a6d6d3a298094a0af139378aef1f64537f90ec
adk2004 <dhingraaksh@gmail.com>
2025-12-13 00:48:15 +0530

    feat: add TypeScript configuration

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from 'dotenv'
 import path from "path";
+import { User } from "db/client";
 
 dotenv.config({path:  path.resolve(__dirname, '../../.env')});
 
